e9fcb169baf8d8d6589589556519d0a87e1fa429
MariemMohamed04 <mariemmohamed1421@gmail.com>
2025-05-28 23:29:27 +0300

    feat(register-api-integration): "Work in progress"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
         <main>
           <Outlet />
         </main>
+        <Toaster position="top-center" />
       </>
     </QueryClientProvider>
   );
