51ff9494f383a5d7941ba25c81f918283ef0014b
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-05-29 19:23:03 +0300

    feat(progress): add progress bar component for step tracking

--- src/app/[locale]/(homepage)/_components/hero/_components/hero.tsx
+++ src/app/[locale]/(homepage)/_components/hero/_components/hero.tsx
@@ -1,7 +1,3 @@
-import ProgressBar from "@/components/feater/progress-bar";
-
 export default function Hero() {
-  return <h1>hero
-    <ProgressBar currentStep={3} />
-  </h1>;
+  return <h1>hero</h1>;
 }
--- src/components/feater/progress-bar.tsx
+++ src/components/feater/progress-bar.tsx
@@ -1,41 +1,12 @@
-
-// import { Progress } from "@radix-ui/react-progress";
-
-// type ProgressCircleProps = {
-//   currentStep: number;
-// };
-
-// const ToaltSteps = 6; 
-// export default function ProgressCircle({ currentStep }: ProgressCircleProps) {
-//   const percentage = (currentStep / ToaltSteps) * 100;
-
-//   return (
-//     <div className="relative w-16 h-16 rounded-full border-2 border-orange-500 flex items-center justify-center">
-//       <div className="">
-//         <Progress 
-//           value={percentage} 
-//           className="h-full w-full rounded-full transform -rotate-90"
-//         />
-//       </div>
-      
-//       <div className="relative z-10 flex flex-col items-center justify-center">
-//         <span className="text-gray font-bold text-lg"> {currentStep}/{ToaltSteps}</span>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 type ProgressCircle = {
   currentStep: number;
 };
-
-const TotalSteps = 6; 
 export default function ProgressCircle({ currentStep }: ProgressCircle) {
-  const percentage = (currentStep / TotalSteps) ;
+  const TotalSteps = 6;
+  const percentage = (currentStep / TotalSteps) * 100;
 
   return (
     <div className="w-16 h-16">
@@ -43,21 +14,11 @@ export default function ProgressCircle({ currentStep }: ProgressCircle) {
         value={percentage}
         text={`${currentStep}/${TotalSteps}`}
         styles={{
-          path: {
-            stroke: `#F97316`, 
-            strokeLinecap: 'round',
-          },
-          trail: {
-            stroke: '#F3F4F6', 
-          },
-          text: {
-            fill: '#111827', 
-            fontSize: '24px',
-            fontWeight: 'bold',
-          },
+          path: { stroke: `#FF4100`, strokeLinecap: 'round', strokeWidth: 4,transition: 'stroke-dashoffset 0.5s ease 0s' },
+          trail: { stroke: '#F3F4F6', strokeWidth: 4,},
+          text: { fill: '#FFFFFF', fontSize: '24px', fontWeight: 'bold' },
         }}
-        strokeWidth={10} 
-              />
+      />
     </div>
   );
 }
