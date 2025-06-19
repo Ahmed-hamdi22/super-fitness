f05cc2fec4789b0b59e44dec7794d1efce2e626c
assmaayaser17 <yaserassmaa@gmail.com>
2025-06-19 16:17:31 +0300

    feat(levels ui): creating modal

--- src/app/[locale]/classes/_components/[primeMoverMuscleId]/page.tsx
+++ src/app/[locale]/classes/_components/[primeMoverMuscleId]/page.tsx
@@ -4,6 +4,7 @@ import ExercisesByLevel from "./Exercises-ui";
 export default function ExercisesPage() {
   // Params
   const { primeMoverMuscleId } = useParams<{ primeMoverMuscleId: string }>();
+  
   return (
     <div>
       {/* Exercise page */}
