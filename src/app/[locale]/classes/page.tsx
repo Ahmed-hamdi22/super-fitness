dbee9f9e58e7f8a403b13c3c176f07274b35fb26
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-06-25 12:43:05 +0300

    solve conflict merge

--- src/app/[locale]/classes/page.tsx
+++ src/app/[locale]/classes/page.tsx
@@ -3,8 +3,11 @@ import DumbbellIcon from "@/components/common/dumbbell";
 import TransformTitle from "@/components/common/transform-title";
 import MuscleGroupList from "../../../components/common/transform-workout";
 import TabFitness from "@/components/common/tab-transform";
+import { useTranslations } from "use-intl";
 
 export default function Classes() {
+  // useTranslation
+  const t = useTranslations();
   return (
     <>
       {/* Main section */}
@@ -13,11 +16,11 @@ export default function Classes() {
         <div className="relative z-10  text-center">
           {/* Header content */}
           <div className="absolute -top-6 left-0 right-0 flex justify-center items-center gap-2 mb-8">
-            <WorkoutsLogo />
+            <WorkoutsLogo text={t("about-us-title")} />
 
             {/* Icon and label for fitness */}
-            <div className="relative z-10 flex items-center gap-2 -bottom-6 ">
-              <DumbbellIcon />
+            <div className="relative z-10 flex items-center gap-2 -bottom-4 capitalize ">
+              <DumbbellIcon text={t("fitness")} />
             </div>
           </div>
 
--- src/components/common/dumbbell.tsx
+++ src/components/common/dumbbell.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function DumbbellIcon({ text }: Props) {
   return (
     <>
-      <Dumbbell className="w-8 h-5 text-flame-orange-500 rotate-45 z-50 underline" />
+      <Dumbbell className="w-12 h-7 text-flame-orange-500 rotate-45 z-50 underline" />
       <span className="text-sm font-semibold text-flame-orange-500">{text}</span>
     </>
   );
