dde65394101768a932ba97eedd5cb56f424942b0
assmaayaser17 <yaserassmaa@gmail.com>
2025-07-10 02:26:44 +0300

    feat(chore/merge-sprint-three):improve updates

--- src/app/[locale]/classes/_components/[primeMoverMuscleId]/Exercises-ui.tsx
+++ src/app/[locale]/classes/_components/[primeMoverMuscleId]/Exercises-ui.tsx
@@ -16,10 +16,9 @@ interface ExercisesPageProps {
 }
 
 export default function ExercisesByLevel({ primeMoverMuscleId }: ExercisesPageProps) {
+  // Translation
+  const t = useTranslations();
 
-    // useTranslation
-    const t = useTranslations();
-    
   // State
   const [selectedLevel, setSelectedLevel] = useState<string | null>(null);
   const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
@@ -54,12 +53,26 @@ export default function ExercisesByLevel({ primeMoverMuscleId }: ExercisesPagePr
     return match && match[2].length === 11 ? match[2] : null;
   }
 
-  // Effect;
+  // Effect
   useEffect(() => {
-    if (levels?.difficulty_levels?.length && levels.difficulty_levels.length > 0) {
+    if (!levels?.difficulty_levels?.length) return;
+
+    const beginnerLevel = levels.difficulty_levels.find(
+      (level) => level.name.toLowerCase() === "beginner",
+    );
+
+    if (beginnerLevel) {
+      setSelectedLevel(beginnerLevel.id);
+    } else {
       setSelectedLevel(levels.difficulty_levels[0].id);
     }
-  }, [levels]);
+  }, [levels]);
+
+  useEffect(() => {
+    if (exercises?.exercises?.length) {
+      setSelectedExercise(exercises.exercises[0]);
+    }
+  }, [exercises]);
 
   // Loading
   if (levelsLoading) {
@@ -71,91 +84,89 @@ export default function ExercisesByLevel({ primeMoverMuscleId }: ExercisesPagePr
     return <div className="text-center py-8 text-red-500">Error While Fetching Levels</div>;
   }
 
-
   return (
-    <div className="dark:bg-dark-gray-900  dark:text-light-silver-300">
+    <div className="w-full h-full px-4 sm:px-6 lg:px-16 py-5 dark:bg-dark-gray-900 dark:text-light-silver-300">
       {/* Header */}
-     <Header />
-     <div className="flex flex-col justify-center items-center relative">
-      <div className=" absolute -top-2 flex justify-center">
-         <WorkoutsLogo text={t("about-us-title")}   />
-      </div>     
-       <TabFitness />
-     </div>
-     
-    <div className=" flex flex-col  lg:flex-row pt-5 gap-6">
-     
-      {/* Exercises */}
-      <div className=" rounded-3xl border-2 border-dark-gray-700 w-full lg:w-[409px] px-3 py-6">
-        {/* Levels */}
-        <div className="flex flex-wrap gap-4 mb-4">
-          {levels?.difficulty_levels?.map((level) => (
-            <button
-              key={level.id}
-              onClick={() => {
-                setSelectedLevel(level.id);
-                setSelectedExercise(null);
-              }}
-              className={`p-3 font-semibold ${
-                selectedLevel === level.id
-                  ? "bg-flame-orange-500 rounded-3xl text-white"
-                  : "text-soft-gray-400"
-              }`}
-            >
-              {level.name}
-            </button>
-          ))}
+      <Header />
+      <div className="flex flex-col justify-center items-center relative ">
+        <div className=" absolute -top-2  flex justify-center pointer-events-none">
+          <WorkoutsLogo text={t("about-us-title")} />
         </div>
+        <TabFitness />
+      </div>
 
+      <div className=" flex flex-col  lg:flex-row pt-5 gap-6 mb-5">
         {/* Exercises */}
-        {exercisesLoading ? (
-          <div className="text-center py-8 text-white">Exercises are Loading....</div>
-        ) : exercisesError ? (
-          <div className="text-center py-8 text-red-500">Error Fetching Exercises</div>
-        ) : exercises?.exercises?.length ? (
-          <div className="space-y-4">
-            {exercises?.exercises?.map((exercise: Exercise) => (
-              
-              <div
-                key={exercise._id}
-                className="p-5 border-b-2 border-dark-gray-700 cursor-pointer"
-                onClick={() => setSelectedExercise(exercise)}
+        <div className=" rounded-3xl border-2 border-dark-gray-700 dark:border-soft-gray-500 w-full lg:w-[409px] px-3 py-6">
+          {/* Levels */}
+          <div className="flex flex-wrap gap-4 mb-4">
+            {levels?.difficulty_levels?.map((level) => (
+              <button
+                key={level.id}
+                onClick={() => {
+                  setSelectedLevel(level.id);
+                  setSelectedExercise(null);
+                }}
+                className={`p-3 font-semibold ${
+                  selectedLevel === level.id
+                    ? "bg-flame-orange-500 rounded-3xl text-white"
+                    : "text-dark-gray-900"
+                }`}
               >
-                <div className="flex gap-4">
-                  <div className="flex justify-between items-center flex-1">
-                    <div>
-                      <h3 className="text-lg font-medium text-soft-gray-60">{exercise.exercise}</h3>
-                      <p className="text-sm font-normal text-soft-gray-60 leading-snug">
-                        {exercise.short_youtube_demonstration}
-                      </p>
-                    </div>
-                    <div className=" bg-flame-orange-500 text-black w-6 h-6 rounded-full flex justify-center items-center">
-                      <Play />
+                {level.name}
+              </button>
+            ))}
+          </div>
+
+          {/* Exercises */}
+          {exercisesLoading ? (
+            <div className="text-center py-8 text-white">Exercises are Loading....</div>
+          ) : exercisesError ? (
+            <div className="text-center py-8 text-red-500">Error Fetching Exercises</div>
+          ) : exercises?.exercises?.length ? (
+            <div className="space-y-4">
+              {exercises?.exercises?.map((exercise: Exercise) => (
+                <div
+                  key={exercise._id}
+                  className="p-5 border-b-2 border-dark-gray-700 dark:border-soft-gray-500 cursor-pointer"
+                  onClick={() => setSelectedExercise(exercise)}
+                >
+                  <div className="flex gap-4">
+                    <div className="flex justify-between items-center flex-1">
+                      <div>
+                        <h3 className="text-lg font-medium text-soft-gray-60">
+                          {exercise.exercise}
+                        </h3>
+                        <p className="text-sm font-normal text-soft-gray-60 leading-snug">
+                          {exercise.short_youtube_demonstration}
+                        </p>
+                      </div>
+                      <div className=" bg-flame-orange-500 text-black w-8 h-8 p-1 rounded-full flex justify-center items-center">
+                        <Play />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ) : (
+              ))}
+            </div>
+          ) : (
+            <div className="text-center py-8 text-white">No Exercises Found</div>
+          )}
+        </div>
 
-          <div className="text-center py-8 text-white">No Exercises Found</div>
-        )}
+        {/* Video */}
+        <div className="flex-1 flex flex-col gap-5">
+          {selectedExercise && (
+            <VideoSection selectedExercise={selectedExercise} extractYoutubeID={extractYoutubeID} />
+          )}
+
+          {/* Icons */}
+          <Icons />
+
+          {/* Card */}
+          <Card />
+        </div>
       </div>
-
-      {/* Video */}
-      <div className="flex-1 flex flex-col gap-5">
-        {selectedExercise && (
-          <VideoSection selectedExercise={selectedExercise} extractYoutubeID={extractYoutubeID} />
-        )}
-
-        {/* Icons */}
-        <Icons />
-
-        {/* Card */}
-        <Card />
-      </div>
-    </div>
     </div>
   );
 }
--- src/app/[locale]/classes/_components/[primeMoverMuscleId]/icons-ui.tsx
+++ src/app/[locale]/classes/_components/[primeMoverMuscleId]/icons-ui.tsx
@@ -2,16 +2,21 @@ import { ArrowRight } from "lucide-react";
 
 export default function Icons() {
   return (
-    <div className="flex items-center gap-8">
+    <div className="flex items-center gap-8 ">
       {[...Array(3)].map((_, index) => (
         <div key={index} className="flex items-center gap-4 pl-7">
           {/* Icon*/}
-          <div className="bg-custom-orange-500 w-9 h-9 flex items-center justify-center rounded-full border-4 border-white">
-            <ArrowRight className="w-5 h-4 text-white" strokeWidth={2.5} />
+          <div className="bg-custom-orange-500 w-9 h-9 flex items-center justify-center rounded-full border-4  border-black dark:border-light-silver-300">
+            <ArrowRight
+              className="w-5 h-4 text-black dark:text-light-silver-300 "
+              strokeWidth={2.5}
+            />
           </div>
-          
+
           {/* Text */}
-          <p className="text-base font-bold text-white">Expertly designed workout.</p>
+          <p className="text-base font-bold text-black dark:text-light-silver-300">
+            Expertly designed workout.
+          </p>
         </div>
       ))}
     </div>
