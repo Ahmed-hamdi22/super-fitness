de6edfe3751a6da2c2d34b1b96ef9c99b72cdcbb
rana-m-h <rana29638@gmail.com>
2025-07-10 19:36:34 +0300

    chore/(merge-sprint-three):Applying some enhancements.

--- src/routers.tsx
+++ src/routers.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: "about", element: <AboutPage /> },
       { path: "healthy", element: <HealthyPage /> },
-      { path: "/mealdetails/:id", element: <Mealdetails /> },
+      { path: "/healthy/:id", element: <Mealdetails /> },
       { path: "classes", element: <Classes /> },
       { path: "account", element: <AccountModal /> },
       { path: "settings", element: <SettingsPage /> },
